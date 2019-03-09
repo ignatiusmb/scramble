c44c4234bc640b087c1b93ccb2c31ca8800f6ea4
Ignatius Bagus <ignatius.mbs@gmail.com>
2019-03-09 15:35:03 +0700

    Add docs-beta

---
+++ docs/index.js
@@ -0,0 +1,8 @@
+const scrambleGroup = document.getElementsByClassName("scramble-group");
+
+function decodeNext(group, index) {
+
+}
+for (let i = 0; i < scrambleGroup.length; i++) {
+  decodeNext(scrambleGroup[i], 0);
+}
