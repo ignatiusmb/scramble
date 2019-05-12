7e4fe5618141a123396d06b3949c81243868077e
Ignatius Bagus <ignatius.mbs@gmail.com>
2019-05-13 02:30:44 +0700

    Refactor createSection and add createButtons

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ for (let i = 0; i < scrambleGroup.length; i++) {
   successiveScramble(group);
 }
 
-const createTerminal = () => {
+const createTerminal = name => {
   const terminal = document.createElement('div');
 
   const header = document.createElement('div');
@@ -22,6 +22,7 @@ const createTerminal = () => {
     if (button.includes('terminal')) {
       const headerName = document.createElement('span');
       headerName.className = 'terminal';
+      headerName.textContent = name;
       header.appendChild(headerName);
     }
   }
@@ -33,56 +34,30 @@ const createTerminal = () => {
   body.appendChild(firstLine);
   terminal.appendChild(body);
 
-  return { window: terminal, head: header, body: body };
+  return { window: terminal, name: name, head: header, body: body };
 };
 
-const scrCreateSection = (name, buttonNames) => {
-  const section = document.createElement('section');
-
-  const title = document.createElement('h2');
-  section.appendChild(title);
-
-  const divDemo = document.createElement('div');
-  divDemo.className = 'demo';
-  const headline = document.createElement('div');
-  headline.className = 'headline';
-  let example = document.createElement('div');
-  example.className = 'example';
-  const status = document.createElement('aside');
-  headline.appendChild(example);
-  headline.appendChild(status);
-  divDemo.appendChild(headline);
-  const buttons = document.createElement('div');
-  buttons.className = 'buttons';
-
-  const terminal = createTerminal();
-
-  const codeblock = document.createElement('pre');
-  const code = document.createElement('code');
-  code.textContent = 'disorder(HTMLElement)';
-  codeblock.appendChild(code);
-  switch (name) {
+const scrCreateButtons = (element, terminal, buttons) => {
+  const buttonContainer = document.createElement('div');
+  switch (terminal.name) {
     case 'jumble':
       break;
     case 'disorder':
-      title.textContent = 'continuous text disorder';
-      const btnName = buttonNames;
-      example.textContent = 'this text is in disorder';
-      example = disorder(example);
+      example = disorder(element);
       status.className = 'running';
-      for (let i = 0; i < btnName.length; i++) {
-        const btn = document.createElement('a');
-        btn.textContent = btnName[i];
+      for (let i = 0; i < buttons.length; i++) {
+        const button = document.createElement('a');
+        button.textContent = buttons[i];
         switch (i) {
           case 0:
-            btn.addEventListener('click', () => {
+            button.addEventListener('click', () => {
               terminal.body.lastChild.textContent = example.original;
               terminal.body.appendChild(document.createElement('code'));
               if (terminal.childElementCount > 10) document.getElementById('clearlog').click();
             });
             break;
           case 1:
-            btn.addEventListener('click', () => {
+            button.addEventListener('click', () => {
               const fooEx = example.process();
               if (fooEx === undefined) return;
               status.className = 'running';
@@ -95,30 +70,72 @@ const scrCreateSection = (name, buttonNames) => {
             });
             break;
           case 2:
-            btn.addEventListener('click', () => {
+            button.addEventListener('click', () => {
               example.start();
               status.className = 'running';
             });
             break;
           case 3:
-            btn.addEventListener('click', () => {
+            button.addEventListener('click', () => {
               example.stop();
               status.className = 'idle';
             });
         }
-        buttons.appendChild(btn);
+        buttonContainer.appendChild(button);
       }
       break;
   }
+  return buttonContainer;
+};
+
+const scrCreateSection = (name, titleText, exampleText, buttonNames) => {
+  const section = document.createElement('section');
+
+  const title = document.createElement('h2');
+  title.textContent = titleText;
+  section.appendChild(title);
+
+  const divDemo = document.createElement('div');
+  divDemo.className = 'demo';
+
+  const headline = document.createElement('div');
+  headline.className = 'headline';
+
+  let example = document.createElement('div');
+  example.textContent = exampleText;
+  example.className = 'example';
+
+  const status = document.createElement('aside');
+  headline.appendChild(example);
+  headline.appendChild(status);
+  divDemo.appendChild(headline);
+
+  const terminal = createTerminal(name);
+
+  const buttons = scrCreateButtons(example, terminal, buttonNames);
+  buttons.className = 'buttons';
   divDemo.appendChild(buttons);
+
+  const codeBlock = document.createElement('pre');
+  const code = document.createElement('code');
+  code.textContent = 'disorder(HTMLElement)';
+  codeBlock.appendChild(code);
+
   divDemo.appendChild(terminal.window);
-  divDemo.appendChild(codeblock);
+  divDemo.appendChild(codeBlock);
   section.appendChild(divDemo);
   return section;
 };
 
 const main = document.getElementById('sections');
-main.appendChild(scrCreateSection('disorder', ['original', 'process', 'start', 'stop']));
+main.appendChild(
+  scrCreateSection('disorder', 'continuous text disorder', 'this text is in disorder', [
+    'original',
+    'process',
+    'start',
+    'stop'
+  ])
+);
 
 fetch('https://cdn.jsdelivr.net/gh/ignatiusmb/ignatiusmb.github.io/dist/footer.html')
   .then(response => {
