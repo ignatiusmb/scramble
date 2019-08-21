3bd6bb25675ba865485da85f2a8fe0308ab7a032
Ignatius Bagus <8156777+ignatiusmb@users.noreply.github.com>
2019-08-21 14:38:37 +0700

    Merge prepared dev branch for v3.0.0 (#3)
    * Update README.md
    
    * Remove richJS script
    
    * Add createInstance method
    
    * Add whitespace to cdn code block
    
    * Increase RANDOM_TIME
    
    * Refactor structure for app injection
    
    * Refactor loop to individual functions
    
    * Remove nav style and change terminal header
    
    * Refactor createSection and add createButtons
    
    * Add reset button styling
    
    * Add reset button
    
    * Refactor injecting section to own function
    
    * Fix main const and move name to pseudo element
    
    * Use standard and remove semicolons
    
    * Update repository, add author and homepage
    
    * Add files section
    
    * Add gitignore from express
    
    * Use eslint dev
    
    * Add eslintrc
    
    * Rename to Scramble and add quick start
    
    * Add node param to env and parseroption set to 2019
    
    * Add run command to quick start
    
    * Add eslint ignore for html
    
    * Add node_modules to eslint ignore
    
    * Update packages and main files
    
    * Create source index for original code and development
    
    * Add build file
    
    * Add built files
    
    * Update license name

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
-/*!
- * ScrambleJS v2.1.4
- * Copyright(c) 2019 Ignatius Bagus
- * MIT Licensed
- * scramble.js.org
- */
-
-'use strict';
-
 // Utility
 /**
  * Check if an element is visible in viewport
  * @param {HTMLElement} elem element to be checked
  */
 function inViewport(elem) {
-  const rect = elem.getBoundingClientRect();
+  const rect = elem.getBoundingClientRect()
   return (
     rect.bottom > 0 &&
     rect.right > 0 &&
@@ -28,9 +19,9 @@ function inViewport(elem) {
  * @param {number} firstChar for a specific first char
  */
 function jumble(textLen) {
-  let generated = '';
-  const randomChar = () => String.fromCharCode(scramble.random(33, 126));
-  for (let i = 0; i < textLen; i++) generated += randomChar();
+  let generated = ''
+  const randomChar = () => String.fromCharCode(scramble.random(33, 126))
+  for (let i = 0; i < textLen; i++) generated += randomChar()
   return generated
 }
 
@@ -41,7 +32,7 @@ function jumble(textLen) {
  * @param {number} firstChar a specific first char index
  */
 function decode(original, decodeLen) {
-  const newText = original.substring(0, decodeLen);
+  const newText = original.substring(0, decodeLen)
   return newText + jumble(original.length - decodeLen)
 }
 
@@ -50,25 +41,25 @@ function decode(original, decodeLen) {
  * @param {HTMLElement} el html element to scramble
  */
 function disorder(el) {
-  const original = el.textContent;
-  const totalChars = original.length;
+  const original = el.textContent
+  const totalChars = original.length
 
-  const execute = () => (el.textContent = jumble(totalChars));
+  const execute = () => (el.textContent = jumble(totalChars))
 
-  let jumbler = setInterval(execute, scramble.interval);
-  let running = true;
+  let jumbler = setInterval(execute, scramble.interval)
+  let running = true
   return {
     original,
     start: () => {
       if (!running) {
-        running = true;
-        jumbler = setInterval(execute, scramble.interval);
+        running = true
+        jumbler = setInterval(execute, scramble.interval)
       } else throw new Error('Instance is already running!')
     },
     stop: () => {
       if (!running) return
-      running = false;
-      clearInterval(jumbler);
+      running = false
+      clearInterval(jumbler)
     }
   }
 }
@@ -81,13 +72,13 @@ function successive(elements) {
   const execute = idx => {
     if (idx >= next.length) return
     function check() {
-      if (next[idx].finished()) execute(idx + 1);
-      else setTimeout(check, 1000);
+      if (next[idx].finished()) execute(idx + 1)
+      else setTimeout(check, 1000)
     }
-    next[idx].run();
-    setTimeout(check, 1000);
-  };
-  const next = Array.from(elements).map(el => scramble(el));
+    next[idx].run()
+    setTimeout(check, 1000)
+  }
+  const next = Array.from(elements).map(el => scramble(el))
   return {
     run: () => execute(0)
   }
@@ -101,62 +92,62 @@ function successive(elements) {
 function scramble(el, args) {
   if (el == null) return
   if (el instanceof NodeList) {
-    const disarray = Array.from(el).map(x => scramble(x));
-    const executed = Array(el.length).fill(false);
+    const disarray = Array.from(el).map(x => scramble(x))
+    const executed = Array(el.length).fill(false)
 
-    const events = ['load', 'scroll'];
+    const events = ['load', 'scroll']
     for (let i = 0; i < el.length; i++) {
       for (const ev of events) {
         window.addEventListener(ev, function check() {
-          if (executed[i]) window.removeEventListener(ev, check);
+          if (executed[i]) window.removeEventListener(ev, check)
           if (!executed[i] && inViewport(el[i])) {
-            executed[i] = true;
-            disarray[i].run();
+            executed[i] = true
+            disarray[i].run()
           }
-        });
+        })
       }
     }
     return {
       status: () => executed.every(x => x)
     }
   } else if (el instanceof HTMLElement) {
-    let executed = false;
-    let letterIdx = 0;
-    let original;
+    let executed = false
+    let letterIdx = 0
+    let original
 
-    if (args === undefined) original = el.textContent;
-    else original = args.original;
+    if (args === undefined) original = el.textContent
+    else original = args.original
 
-    const totalChars = original.length;
-    const runner = disorder(el);
+    const totalChars = original.length
+    const runner = disorder(el)
 
-    let timer;
+    let timer
     const iterateLetters = () => {
-      clearInterval(timer);
+      clearInterval(timer)
       timer = setInterval(() => {
-        if (letterIdx >= totalChars) clearInterval(timer);
-        el.textContent = decode(original, letterIdx);
-      }, scramble.interval);
-      if (letterIdx++ >= totalChars) clearTimeout(iterateLetters);
-      else setTimeout(iterateLetters, 432);
-    };
+        if (letterIdx >= totalChars) clearInterval(timer)
+        el.textContent = decode(original, letterIdx)
+      }, scramble.interval)
+      if (letterIdx++ >= totalChars) clearTimeout(iterateLetters)
+      else setTimeout(iterateLetters, 432)
+    }
     return {
       finished: () => letterIdx >= totalChars,
       run: () => {
         if (executed) return
-        executed = true;
-        runner.stop();
-        iterateLetters();
+        executed = true
+        runner.stop()
+        iterateLetters()
       },
       worker: runner
     }
   }
 }
 
-scramble.interval = 42;
-scramble.disorder = disorder;
-scramble.successive = successive;
+scramble.interval = 42
+scramble.disorder = disorder
+scramble.successive = successive
 
-scramble.random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+scramble.random = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min
 
-module.exports = scramble;
+export default scramble
