f362a16f0a5d1a019ee7d2a8cd1ed6e795a9dc80
Ignatius Bagus <ignatius.mbs@gmail.com>
2019-09-29 13:55:31 +0700

    Move onload scripts to head as async adn defer

--- src/onload.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', () => {
-  for (const url of ['lib/scramble.min.js', 'src/gh-pages.js']) {
-    const script = document.createElement('script')
-    script.src = url
-    document.body.appendChild(script)
-  }
-})
