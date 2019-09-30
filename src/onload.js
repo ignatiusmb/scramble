a37221b4a0c7d0962b1ce6371af9f815170704ce
Ignatius Bagus <ignatius.mbs@gmail.com>
2019-09-30 21:36:12 +0700

    Update to new cdn link

--- src/onload.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', () => {
-  const script = document.createElement('script')
-  script.type = 'text/javascript'
-  script.src = 'src/gh-pages.js'
-  document.body.appendChild(script)
-})
